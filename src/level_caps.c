2af112b238dc72a9ef5bd1ad93eb98d7b8ed04f0
DaDainDan <lelouchismywaifu@gmail.com>
2024-11-29 08:33:24 -0600

    Littleroot Fix and Level Caps

--- src/level_caps.c
+++ src/level_caps.c
@@ -9,15 +9,16 @@ u32 GetCurrentLevelCap(void)
 {
     static const u32 sLevelCapFlagMap[][2] =
     {
-        {FLAG_BADGE01_GET, 15},
-        {FLAG_BADGE02_GET, 19},
-        {FLAG_BADGE03_GET, 24},
-        {FLAG_BADGE04_GET, 29},
-        {FLAG_BADGE05_GET, 31},
-        {FLAG_BADGE06_GET, 33},
-        {FLAG_BADGE07_GET, 42},
-        {FLAG_BADGE08_GET, 46},
-        {FLAG_IS_CHAMPION, 58},
+        {FLAG_BADGE01_GET, 12},
+        {FLAG_BADGE02_GET, 20},
+        {FLAG_BADGE03_GET, 29},
+        {FLAG_BADGE04_GET, 38},
+        {FLAG_BADGE05_GET, 46},
+        {FLAG_BADGE06_GET, 57},
+        {FLAG_BADGE07_GET, 69},
+        {FLAG_BADGE08_GET, 79},
+        {FLAG_DEFEATED_WALLY_VICTORY_ROAD, 90}
+        {FLAG_IS_CHAMPION, 100},
     };
 
     u32 i;
